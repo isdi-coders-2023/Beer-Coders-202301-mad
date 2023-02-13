f60d1f59cb8e7d17785be958a00ca52cd8163691
Leandro <leandrolupano@hotmail.com>
2023-02-13 19:51:47 +0100

    ✅ Add App test of Footer

--- src/core/components/footer/footer.tsx
+++ src/core/components/footer/footer.tsx
@@ -1,35 +1,37 @@
 import './footer.scss';
 export function Footer() {
   return (
-<footer className="container__footer">
-    <div>
-      <div className="footer-title"> <img src="./img/logo2-transparent.png" alt="logo" height="30px" width="40px">Beers
-        City
-        <p className="title-small">Copyright © Beers City 2020 All rights reserved</p>
-
+    <footer className="container__footer">
+      <div>
+        <div className="footer-title">
+          {' '}
+          <img
+            src="./img/logo2-transparent.png"
+            alt="logo"
+            height="30px"
+            width="40px"
+          />
+          Beers City
+          <p className="title-small">
+            Copyright © Beers City 2020 All rights reserved
+          </p>
+        </div>
       </div>
-    </div>
-    <ul>
-      <li className="small-menu">Contac Info</li>
-      <li className="menu-list">+123 456 789</li>
-      <li className="menu-list">Info@BeersCity.com</li>
-      <li className="menu-list">1245, Madrid, Spain</li>
-    </ul>
-    <div className="qlq">
+      <ul>
+        <li className="small-menu">Contac Info</li>
+        <li className="menu-list">+123 456 789</li>
+        <li className="menu-list">Info@BeersCity.com</li>
+        <li className="menu-list">1245, Madrid, Spain</li>
+      </ul>
+      <div className="qlq">
+        <p className="small-menu">Follow us on</p>
 
-      <p className="small-menu">Follow us on
-
-      </p>
-
-      <div className="list-logo">
-        <img src="./img/facebook-green.png" alt="facebook" width="15px">
-        <img src="./img/instagram-green.png" alt="instagram" width="15px">
-        <img src="./img/twitter-green.png" alt="Twitter" width="15px">
+        <div className="list-logo">
+          <img src="./img/facebook-green.png" alt="facebook" width="15px" />
+          <img src="./img/instagram-green.png" alt="instagram" width="15px" />
+          <img src="./img/twitter-green.png" alt="Twitter" width="15px" />
+        </div>
       </div>
-    </div>
-  </footer>
-
-
-
+    </footer>
   );
 }
