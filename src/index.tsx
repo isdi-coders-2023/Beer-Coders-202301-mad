ed4232aea439b8d0faccb525723f232e0127d560
gonzu <gonzalodiezdavila@gmail.com>
2023-02-13 19:06:09 +0100

    🎨 Add header component

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './core/components/app/App';
+import { Header } from './core/components/header/header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Header></Header>
   </React.StrictMode>
 );
