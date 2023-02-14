4c68390b242262f1d07edafe71be3a67515d2819
Leandro <leandrolupano@hotmail.com>
2023-02-14 19:46:15 +0100

    🐛 Fix code smell

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './core/components/app/App';
-import { BeerApiRepo } from './features/services/beer.api.repo';
 import './index.scss';
 
 const root = ReactDOM.createRoot(
