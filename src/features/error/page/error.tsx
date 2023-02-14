9f9be32160ae859183b8cc5344b1d92e8615a7ae
Jesus <jesus.cassano.2006@gmail.com>
2023-02-14 19:16:11 +0100

    test  changes

---
+++ src/features/error/page/error.tsx
@@ -0,0 +1,10 @@
+import './error.scss';
+
+export default function ErrorPage() {
+  return (
+    <section className="container-error">
+      <h2>PAGE NOT FOUND 404</h2>
+      <img src="./img/elf-logo2-transparent.png" alt="elf drink"></img>
+    </section>
+  );
+}
