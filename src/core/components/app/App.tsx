5a4368dabb17ec4d2bef8aa7882f5c7114d12734
Leandro <leandrolupano@hotmail.com>
2023-02-14 12:58:33 +0100

    correct audit error

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -16,9 +16,9 @@ function App() {
   return (
     <div className="App">
       {/* TEMPORAL:
-       <Header>
-        <Menu menuOptions={menuOptions}></Menu>
-      </Header> */}
+        <Header>
+          <Menu menuOptions={menuOptions}></Menu>
+        </Header> */}
       <AppRouter menuOptions={menuOptions}></AppRouter>
       <Footer></Footer>
     </div>
