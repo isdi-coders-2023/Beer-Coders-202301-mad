00769860985840a7542c2114f22dfaf4a4868ba6
Leandro <leandrolupano@hotmail.com>
2023-02-14 10:22:18 +0100

    🐛 Correct html and app.router bugs

--- src/core/components/app.router/app.router.tsx
+++ src/core/components/app.router/app.router.tsx
@@ -2,9 +2,10 @@ import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MenuOption } from '../app/App';
 
-const Home = lazy(() => import('../../../features/home/page/home'));
-const MyBeers = lazy(() => import('../../../features/todo/page/todo'));
-const About = lazy(() => import('../../../features/about/page/about'));
+// COMENTARIO TEMPORAL HASTA TERMINAR HOME, MY BEERS Y ABOUT.
+// const Home = lazy(() => import('../../../features/home/page/home'));
+// const MyBeers = lazy(() => import('../../../features/mybeers/page/mybeers'));
+// const About = lazy(() => import('../../../features/about/page/about'));
 
 type AppRouterProps = {
   menuOptions: MenuOption[];
@@ -14,11 +15,11 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
   return (
     <Suspense>
       <Routes>
-        <Route path={'/'} element={<Home></Home>}></Route>
+        {/* <Route path={'/'} element={<Home></Home>}></Route>
         <Route path={menuOptions[0].path} element={<Home></Home>}></Route>
         <Route path={menuOptions[1].path} element={<MyBeers></MyBeers>}></Route>
         <Route path={menuOptions[2].path} element={<About></About>}></Route>
-        <Route path={'*'} element={<ErrorPage></ErrorPage>}></Route>
+        <Route path={'*'} element={<ErrorPage></ErrorPage>}></Route> */}
       </Routes>
     </Suspense>
   );
