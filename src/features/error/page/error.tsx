2eaa90f17924a994e020615f4a6397d1976972fa
Jesus <jesus.cassano.2006@gmail.com>
2023-02-15 11:02:05 +0100

    📈 add error files

--- src/features/error/page/error.tsx
+++ src/features/error/page/error.tsx
@@ -1,10 +1,24 @@
 import './error.scss';
 
-export default function ErrorPage() {
+export default function Error(): JSX.Element {
   return (
     <section className="container-error">
-      <h2>PAGE NOT FOUND 404</h2>
-      <img src="./img/elf-logo2-transparent.png" alt="elf drink"></img>
+      <div className="error-text">
+        <div>
+          <p className="number">404</p>
+          <p className="page"> PAGE NOT FOUND</p>
+        </div>
+      </div>
+      <div className="error-logo">
+        <div>
+          <img
+            src="./img/elf-logo-transparent.png"
+            alt="elf drink"
+            height="100px"
+            width="100px"
+          ></img>
+        </div>
+      </div>
     </section>
   );
 }
