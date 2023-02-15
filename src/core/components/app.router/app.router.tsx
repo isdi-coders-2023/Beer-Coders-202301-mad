cd4ce0a11a55bdf49f091070c27f805c7b97c9c9
Jesus <jesus.cassano.2006@gmail.com>
2023-02-15 15:36:46 +0100

    approuter modify

--- src/core/components/app.router/app.router.tsx
+++ src/core/components/app.router/app.router.tsx
@@ -4,8 +4,7 @@ import { MenuOption } from '../app/App';
 
 const Home = lazy(() => import('../../../features/home/page/home'));
 
-// COMENTARIO TEMPORAL HASTA TERMINAR HOME, MY BEERS Y ABOUT.
-// const MyBeers = lazy(() => import('../../../features/mybeers/page/mybeers'));
+const MyBeers = lazy(() => import('../../../features/mybeers/page/mybeers'));
 const About = lazy(() => import('../../../features/about/page/about'));
 const ErrorPage = lazy(() => import('../../../features/error/page/error'));
 const Details = lazy(() => import('../../../features/details/page/details'));
@@ -20,7 +19,7 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
       <Routes>
         <Route path={'/'} element={<Home></Home>}></Route>
         <Route path={menuOptions[0].path} element={<Home></Home>}></Route>
-        {/* <Route path={menuOptions[1].path} element={<MyBeers></MyBeers>}></Route> */}
+        <Route path={menuOptions[1].path} element={<MyBeers></MyBeers>}></Route>
         <Route path={menuOptions[2].path} element={<About></About>}></Route>
         <Route path={'*'} element={<ErrorPage></ErrorPage>}></Route>
         {/* <Route path={'*'} element={<ErrorPage></ErrorPage>}></Route> */}
