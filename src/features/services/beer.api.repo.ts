4ae90114d8d92702015d47649214c6d2bfd7b805
Leandro <leandrolupano@hotmail.com>
2023-02-15 10:39:08 +0100

    👷 Add load y get de Api Repo

--- src/features/services/beer.api.repo.ts
+++ src/features/services/beer.api.repo.ts
@@ -1,5 +1,10 @@
 import { BeerStructure } from '../models/beer';
 
+export interface BeerApiRepoStructure {
+  loadBeers(): Promise<BeerStructure[]>;
+  getBeer(id: BeerStructure['id']): Promise<BeerStructure>;
+}
+
 export class BeerApiRepo {
   url: string;
 
@@ -12,6 +17,7 @@ export class BeerApiRepo {
     const beersPerPage = '&per_page=20';
 
     const pageNumber = '1';
+
     const url = this.url + defaultPage + pageNumber + beersPerPage;
 
     const resp = await fetch(url);
