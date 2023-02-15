90deca06bdd53ab3d8f1b2840a4b7a2178dbd22a
Jesus <jesus.cassano.2006@gmail.com>
2023-02-15 15:37:32 +0100

    💄 mybeers add file

---
+++ src/features/mybeers/page/mybeers.tsx
@@ -0,0 +1,9 @@
+import './mybeers.scss';
+import { DetailedCard } from '../../../core/components/detailsCard/detailsCard';
+export default function MyBeers() {
+  return (
+    <>
+      <h2>My Beers</h2>;<MyBeers></MyBeers>;
+    </>
+  );
+}
