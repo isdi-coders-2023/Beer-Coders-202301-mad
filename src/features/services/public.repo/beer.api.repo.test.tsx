18fedc46df813bf76898cdca5043fe2de7299a92
Leandro <leandrolupano@hotmail.com>
2023-02-16 14:44:29 +0100

    ✅ Add public repo test

---
+++ src/features/services/public.repo/beer.api.repo.test.tsx
@@ -0,0 +1,31 @@
+import { BeerApiRepo } from './beer.api.repo';
+
+describe('Given there BeerApiRepo class', () => {
+  describe('When we intance the class with new BeerApiRepo', () => {
+    let repo: BeerApiRepo;
+
+    beforeEach(() => {
+      repo = new BeerApiRepo();
+    });
+
+    test('Then if we call the loadBeer() method, the result should be equal to the mock value', async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue([]),
+      });
+
+      const result = await repo.loadPublicBeers();
+      expect(result).toEqual([]);
+    });
+
+    test('Then if we call the getBeer() method, the result should be equal to the mock value', async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue({
+          name: '',
+        }),
+      });
+
+      const result = await repo.getPublicBeer(1);
+      expect(result).toEqual({ name: '' });
+    });
+  });
+});
