2f52abb9b80229601e88fcb4d425bad756fe92eb
Leandro <leandrolupano@hotmail.com>
2023-02-16 15:44:36 +0100

    🐛 Fix custom hook bug

--- src/beers/hooks/use.beers.tsx
+++ src/beers/hooks/use.beers.tsx
@@ -28,33 +28,6 @@ export function useBeers(repo: BeerApiRepo) {
     }
   }, [repo]);
 
-  // const addBeer = async (beer: ProtoBeerStructure) => {
-  //   try {
-  //     const finalBeer = await repo.createBeer(beer);
-  //     dispatch(action.addBeersCreator(finalBeer));
-  //   } catch (error) {
-  //     handlerError(error as Error);
-  //   }
-  // };
-
-  // const deleteBeer = async (id: BeerStructure['id']) => {
-  //   try {
-  //     await repo.delete(id);
-  //     dispatch(action.deleteBeersCreator(id));
-  //   } catch (error) {
-  //     handlerError(error as Error);
-  //   }
-  // };
-
-  // const updateBeer = async (beer: BeerStructure) => {
-  //   try {
-  //     const finalNote = await repo.update(beer);
-  //     dispatch(action.updateBeersCreator(finalNote));
-  //   } catch (error) {
-  //     handlerError(error as Error);
-  //   }
-  // };
-
   return {
     beerList,
     loadBeers,
