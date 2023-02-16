e2f6dc88dd7791dd8182243f4d995fe8ec94b58f
Leandro <leandrolupano@hotmail.com>
2023-02-16 21:58:55 +0100

    🐛 Correct app router test

--- src/core/components/app.router/app.router.test.tsx
+++ src/core/components/app.router/app.router.test.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import { MemoryRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { MenuOption } from '../app/App';
 import { AppRouter } from './app.router';
 import '@testing-library/jest-dom';
+import { UseBeersStructure } from '../../../beers/hooks/use.beers';
+import { BeersContext } from '../../../beers/context/beers.context';
 
 describe('Given AppRouter component', () => {
   const mockOptions: MenuOption[] = [
@@ -21,23 +24,33 @@ describe('Given AppRouter component', () => {
     },
   ];
 
-  let counter = 0;
+  const mockContext = {
+    loadBeers: jest.fn(),
+    beerList: [{ name: 'test' }],
+  } as unknown as UseBeersStructure;
 
-  const prepareTestFunction = () => {
+  const prepareTestFunction = (number: number) => {
     render(
-      <Router
-        initialEntries={['/home', '/about', '/details', '/anyText', '/mybeers']}
-        initialIndex={counter}
-      >
-        <AppRouter menuOptions={mockOptions}></AppRouter>
-      </Router>
+      <BeersContext.Provider value={mockContext}>
+        <Router
+          initialEntries={[
+            '/home',
+            '/about',
+            '/details',
+            '/anyText',
+            '/mybeers',
+          ]}
+          initialIndex={number}
+        >
+          <AppRouter menuOptions={mockOptions}></AppRouter>
+        </Router>
+      </BeersContext.Provider>
     );
-    counter++;
   };
 
   describe('When it is render and the path is "/home"', () => {
     test('Then, the title "Discover" of Home Page should be in the screen', async () => {
-      prepareTestFunction();
+      prepareTestFunction(0);
       const element = await screen.findByRole('heading', { name: 'Discover' });
       expect(element).toBeInTheDocument();
     });
@@ -45,7 +58,7 @@ describe('Given AppRouter component', () => {
 
   describe('When it is render and the path is "/about"', () => {
     test('Then, the title "About us" of About page  should be in the screen', async () => {
-      prepareTestFunction();
+      prepareTestFunction(1);
       const element = await screen.findByRole('heading', {
         name: 'About us',
       });
@@ -55,7 +68,7 @@ describe('Given AppRouter component', () => {
 
   describe('When it is render the Details page', () => {
     test('Then, it should render the Details component', async () => {
-      prepareTestFunction();
+      prepareTestFunction(2);
       const element = await screen.findByRole('heading', {
         name: 'Details',
       });
@@ -65,7 +78,7 @@ describe('Given AppRouter component', () => {
 
   describe('When it is render the Error page', () => {
     test('Then, it should render the Error component', async () => {
-      prepareTestFunction();
+      prepareTestFunction(3);
       const element = await screen.findByText(/404/i);
       expect(element).toBeInTheDocument();
     });
@@ -73,7 +86,7 @@ describe('Given AppRouter component', () => {
 
   describe('When it is render and the path is "/mybeers"', () => {
     test('Then, the title "My Beers" of My Beers Page should be in the screen', async () => {
-      prepareTestFunction();
+      prepareTestFunction(4);
       const element = await screen.findByRole('heading', {
         name: 'My Beers',
       });
