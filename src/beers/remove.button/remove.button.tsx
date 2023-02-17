383556e6a60b514aa7092d809c80bdbf2b19602e
Jesus <jesus.cassano.2006@gmail.com>
2023-02-17 09:04:39 +0100

    ✅ remove button add

--- src/beers/remove.button/remove.button.tsx
+++ src/beers/remove.button/remove.button.tsx
@@ -1,8 +1,18 @@
+import { DetailedCard } from '../../core/components/detailsCard/detailsCard';
+
 export default function Remove(): JSX.Element {
+  function deleteBeers(name: any) {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <>
       <div className="remove-button">
-        <button onClick={Remove}>
+        <button
+          onClick={() => {
+            deleteBeers(DetailedCard.name);
+          }}
+        >
           {' '}
           <img src="img/remove-icon.png" alt="remove button" height="20rem" />
         </button>
