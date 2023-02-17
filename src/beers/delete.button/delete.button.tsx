4661aa43d59e893680cf860d5d163a29fc82981a
Jesus <jesus.cassano.2006@gmail.com>
2023-02-17 18:50:09 +0100

    ✏️ fix files buttons

---
+++ src/beers/delete.button/delete.button.tsx
@@ -0,0 +1,12 @@
+export default function DeleteButton(): JSX.Element {
+  return (
+    <>
+      <div className="delete-button">
+        <button>
+          {' '}
+          <img src="img/delete-icon.png" alt="delete button" height="20rem" />
+        </button>
+      </div>
+    </>
+  );
+}
