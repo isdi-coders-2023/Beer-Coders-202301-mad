6ab44644191e8a8201f87a9f985d0c977002e044
Leandro <leandrolupano@hotmail.com>
2023-02-17 13:45:11 +0100

    ➕ Create partial of add button to MainCard

--- src/beers/home/components/main.card/main.card.tsx
+++ src/beers/home/components/main.card/main.card.tsx
@@ -1,9 +1,23 @@
+import { useContext } from 'react';
+import { BeersContext } from '../../../context/beers.context';
 import { BeerStructure } from '../../../models/beer';
 import './main.card.scss';
 
 type MainCardProps = {
   beer: BeerStructure;
 };
+
+// TEMPORAL: Hasta que esté el Custom Hook Private:
+// const { createBeer } = useContext(BeersContext);
+
+// const handlerAddBeer = (event: SyntheticEvent) => {
+//   event.preventDefault();
+//   const form = event.target as HTMLFormElement;
+//   const inputs = form.querySelectorAll('input');
+//   const newNote = new ProtoNote(inputs[0].value, inputs[1].value);
+//   console.log(newNote);
+//   addNote(newNote);
+// };
 
 export function MainCard({ beer }: MainCardProps) {
   return (
@@ -13,7 +27,12 @@ export function MainCard({ beer }: MainCardProps) {
         <p className="info__name">{beer.name}</p>
         <p className="info__ibu">IBU: {beer.ibu}</p>
       </div>
-      <button className="add-button">
+      <button
+        className="add-button"
+        // onClick={() => {
+        //   createBeer(beer);
+        // }}
+      >
         <img src="img/add-icon.png" alt="Add button" />
       </button>
     </li>
