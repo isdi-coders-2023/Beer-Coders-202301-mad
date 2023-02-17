7a5721573e68bbe8a4a129e816a8e17b938a9cfe
Jesus <jesus.cassano.2006@gmail.com>
2023-02-17 16:23:55 +0100

    ✅ approuter modify file

--- src/core/components/app.router/app.router.tsx
+++ src/core/components/app.router/app.router.tsx
@@ -7,6 +7,8 @@ const MyBeers = lazy(() => import('../../../beers/mybeers/page/mybeers'));
 const About = lazy(() => import('../../../beers/about/page/about'));
 const ErrorPage = lazy(() => import('../../../beers/error/page/error'));
 const Details = lazy(() => import('../../../beers/details/page/details'));
+const EditPage = lazy(() => import('../../../beers/edit/page/editPage'));
+const CreatePage = lazy(() => import('../../../beers/create/page/createPage'));
 
 type AppRouterProps = {
   menuOptions: MenuOption[];
@@ -21,6 +23,8 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
         <Route path={menuOptions[1].path} element={<MyBeers></MyBeers>}></Route>
         <Route path={menuOptions[2].path} element={<About></About>}></Route>
         <Route path={'/details'} element={<Details></Details>}></Route>
+        <Route path={'/editPage'} element={<EditPage></EditPage>}></Route>
+        <Route path={'/createPage'} element={<CreatePage></CreatePage>}></Route>
         <Route path={'*'} element={<ErrorPage></ErrorPage>}></Route>
       </Routes>
     </Suspense>
