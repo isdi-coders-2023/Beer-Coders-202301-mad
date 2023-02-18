7cec86e6afb2a88ce18b6b7df1e1e8ded26612b3
Leandro <leandrolupano@hotmail.com>
2023-02-18 20:33:15 +0100

    ⚰️ Try to complete card details test (fail)

--- src/beers/detailsCard/detailsCard.test.tsx
+++ src/beers/detailsCard/detailsCard.test.tsx
@@ -3,9 +3,11 @@
 import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { PrivateBeersContext } from '../context/private.beer.context';
+//TEMPORAL:  import Edit from '../edit/page/edit';
 import { usePrivateBeersStructure } from '../hooks/hook.private/use.private.beers';
 import { BeerStructure } from '../models/beer';
 import { DetailedCard } from './detailsCard';
+import { MemoryRouter as Router } from 'react-router-dom';
 
 const mockBeer: BeerStructure = {
   name: 'Test beer',
@@ -15,13 +17,17 @@ const mockContext = {
   deleteBeer: jest.fn(),
 } as unknown as usePrivateBeersStructure;
 
+jest.mock('../edit/page/edit');
+
 describe('Given detailsCard component', () => {
   describe('When the component is render', () => {
     beforeEach(async () => {
       await act(async () =>
         render(
           <PrivateBeersContext.Provider value={mockContext}>
-            <DetailedCard beer={mockBeer}></DetailedCard>
+            <Router>
+              <DetailedCard beer={mockBeer}></DetailedCard>
+            </Router>
           </PrivateBeersContext.Provider>
         )
       );
@@ -33,9 +39,16 @@ describe('Given detailsCard component', () => {
     });
 
     test('Then, if the user click on Delte button the deleteBeer function have to been called', async () => {
-      const element = screen.getByRole('button');
-      await act(async () => userEvent.click(element));
+      const elements = screen.getAllByRole('button');
+      await act(async () => userEvent.click(elements[0]));
       expect(mockContext.deleteBeer).toHaveBeenCalled();
     });
+
+    // TEMPORAL: Hasta solucionar el test.
+    // TEMPORAL: test('Then, if the user click on Edit button the Edit component has to been called', async () => {
+    // TEMPORAL:   const element = screen.getByRole('button', { name: 'Edit' });
+    // TEMPORAL:   await act(async () => userEvent.click(element));
+    // TEMPORAL:   expect(Edit).toHaveBeenCalled();
+    // TEMPORAL: });
   });
 });
