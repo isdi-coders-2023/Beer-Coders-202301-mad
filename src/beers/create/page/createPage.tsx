ef5fd99e70c709531f9ec1fec7fbeb1244662e69
Jesus <jesus.cassano.2006@gmail.com>
2023-02-19 20:54:51 +0100

    ⚰️ Remove files duobles

--- src/beers/create/page/createPage.tsx
+++
@@ -1,9 +0,0 @@
-import './createPage.scss';
-
-export default function AddPage() {
-  return (
-    <>
-      <h2>My Beers - Add your own beer</h2>;
-    </>
-  );
-}
--- src/beers/edit/page/editPage.tsx
+++
@@ -1,9 +0,0 @@
-import './editPage.scss';
-
-export default function EditPage() {
-  return (
-    <>
-      <h2>My Beers - Edit</h2>;
-    </>
-  );
-}
